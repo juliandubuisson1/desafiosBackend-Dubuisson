e40f00be23cbcc2e6e711e0270e17e4033e69343
juliandubuisson1 <113142479+juliandubuisson1@users.noreply.github.com>
2024-07-11 18:04:28 -0300

    Update Users.model.js

--- src/dao/models/mongo_models/Users.model.js
+++ src/dao/models/mongo_models/Users.model.js
@@ -6,12 +6,22 @@ const userSchema = new mongoose.Schema({
     email: String,
     age: Number,
     password: String,
+    profile: String,
     cart:
     {
         type: mongoose.Schema.Types.ObjectId,
         ref: "Cart"
     },
     role: { type: String, default: "user" },
+    resetToken: String,
+    resetTokenExpires: Date,
+    documents: [
+        {
+            name: { type: String, required: true },
+            reference: { type: String, required: true }
+        }
+    ],
+    last_connection: Date,
 });
 
 const User = mongoose.model("User", userSchema);
